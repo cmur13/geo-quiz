5183dad6db6433c1934aea7c2e7d05de05a78258
cmur13 <cmur131@gmail.com>
2024-09-05 17:28:16 -0700

    chapter2

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -3,38 +3,58 @@ package com.bignerdranch.android.geoquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import android.widget.Button
-import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+
+    private lateinit var binding: ActivityMainBinding
+
+    // create a list of question objects in MainActivity
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia,true)
+    )
+    // index for the list
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener{
-            //Toast.makeText(
-             //   this,
-             //   R.string.correct_toast,
-             //   Toast.LENGTH_SHORT
-            Snackbar.make(
-                findViewById(R.id.snack),
-                R.string.correct_toast,
-                Snackbar.LENGTH_SHORT
-            ).show()
+        binding.trueButton.setOnClickListener{view: View->
+            checkAnswer(true)
         }
 
-        falseButton.setOnClickListener {
-            Snackbar.make(
-                findViewById(R.id.snack),
-                R.string.incorrect_toast,
-                Snackbar.LENGTH_SHORT
-            ).show()
+        binding.falseButton.setOnClickListener {view:View->
+            checkAnswer(false)
         }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex=(currentIndex+1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+    private fun updateQuestion(){
+        val questionTextResID = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResID)
+    }
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if(userAnswer == correctAnswer){
+            R.string.correct_toast
+        }
+        else{
+            R.string.incorrect_toast
+        }
+        Snackbar.make(findViewById(R.id.snack), messageResId, Snackbar.LENGTH_SHORT)
+            .show()
     }
 }
--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -1,4 +1,4 @@
 package com.bignerdranch.android.geoquiz
 
-class Question {
-}
+import androidx.annotation.StringRes
+data class Question (@StringRes val textResId: Int, val answer: Boolean)
